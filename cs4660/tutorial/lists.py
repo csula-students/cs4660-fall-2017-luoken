488b294708b1dce26d4c43f0b7908f3ff0bedeff
Ken Luo <luozhirong@gmail.com>
2017-09-09 09:15:23 -0700

    deleted some stuff on list.py

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -3,17 +3,14 @@
 def get_first_item(li):
     """Return the first item from the list"""
     return li[0]
-    pass
 
 def get_last_item(li):
     """Return the last item from the list"""
     return li[-1]
-    pass
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
     return li[1:3]
-    pass
 
 def get_sum(li):
     """Return the sum of the list items"""
@@ -21,7 +18,6 @@ def get_sum(li):
     for i in li:
         holder = holder + i
     return holder
-    pass
 
 def get_avg(li):
     """Returns the average of the list items"""
@@ -30,4 +26,3 @@ def get_avg(li):
         holder = holder + i
     answer = holder / len(li)
     return answer
-    pass
