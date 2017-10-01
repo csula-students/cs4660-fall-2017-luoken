b6d5d77bfc0d470b3012939e023e0c805207aa8e
Ken Luo <luozhirong@gmail.com>
2017-09-30 20:36:25 -0700

    added files and lists

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -8,31 +8,48 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
+        f = open(file_path)
+        holder = f.read()
+        self.splitByRow = holder.split("\n")
+        self.numbers = [space.split(' ') for space in self.splitByRow]
+        
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        return self.get_sum(line_number)/(1.0 * len(self.numbers[0]))
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        maximum = self.numbers[0][0]
+        for x in self.numbers[line_number]:
+            if(x > maximum):
+                maximum = x
+        return int(maximum)
+        
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        minimum = self.numbers[0][0]
+        for x in self.numbers[line_number]:
+            if(x < minimum):
+                minimum = x
+        return int(minimum)
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        mySum = 0
+        for x in self.numbers[line_number]:
+            mySum += int(x)
+        return mySum
