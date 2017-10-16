c9a7edb8688fa21336e7d86dcc04b5543f14a5da
Ken Luo <luozhirong@gmail.com>
2017-10-15 18:41:37 -0700

    finished bfs

--- cs4660/quiz/main.py
+++ cs4660/quiz/main.py
@@ -47,55 +47,12 @@ def __json_request(target_url, body):
     response = json.load(urlopen(req, jsondataasbytes))
     return response
 
-
-def bfs(start, end):
-    return True
-    
-
 if __name__ == "__main__":
     # Your code starts here
     start_point = '7f3dc077574c013d98b2de8f735058b4'
     empty_room = get_state(start_point)
-    #end_room = get_state('f1f131f647621a4be7c71292e79613f9')
     end_point = 'f1f131f647621a4be7c71292e79613f9'
-    #end_point = '0d1d67f3e6bf24e4c2acff975025a497'
     end_room = get_state(end_point)
-
-
-    # print(empty_room)
-    # for x in empty_room['neighbors']:
-    #     print(transition_state(empty_room['id'], x['id']))
-    # # print(transition_state(empty_room['id'], empty_room['neighbors'][2]['id']))
-    # print(get_state('f1f131f647621a4be7c71292e79613f9'))
-    # for room in empty_room:
-    #     print(room)
-    # print(empty_room['neighbors'])
-
-    # Q = Queue()
-    # q.put(empty_room)
-    # iterated = []
-    # path = []
-    # parents = {}
-    # #need to get state of new current
-    # while q:
-    #     if(q.empty()):
-    #         break
-    #     current = q.get(0)
-
-    #     for value in get_state(current['id'])['neighbors']:
-    #         for x in current['neighbors']:
-    #             if x not in iterated:
-    #                 iterated.append(x)
-    #             if (bool(parents) == False):
-    #                 parents[current['id']] = None
-    #                 parents[x['id']] = current['id']
-    #             else:
-    #                 if x['id'] not in parents:
-    #                     parents[x['id']] = current['id']
-    #                 #q.put(get_state(x))
-
-    #             print("x ", x['id'])
-    #         print("value ", value['id'])
 
     q = Queue()
     q.put(start_point)
@@ -103,7 +60,10 @@ if __name__ == "__main__":
     iterated = []
     path = []
     parents = {}
-
+    # print(empty_room['location'])['name']
+    # print
+    # print(empty_room['neighbors'])
+    print("BFS")
     while q:
         if(q.empty()):
             break
@@ -123,32 +83,27 @@ if __name__ == "__main__":
             else:
                 if point['id'] not in parents:
                     parents[point['id']] = current
-            print(point)
-            print(point['id'])
             q.put(point['id'])
-            
-
 
     if end_point in parents:
-        print("yes")
-    else:
-        print("no")
-    
-    print("current ", current)
-    print
-    print
-    print("parents ", parents)
-    #need from where? id, to where and effect?
-    print
-    print "start"
-    print
-    print empty_room['id']
-    print
-    print empty_room
-    print
-    print transition_state(empty_room['id'], empty_room['neighbors'][0]['id'])
-    print
-    print empty_room['neighbors'][0]['id']
-    print
-    for x in empty_room['neighbors']:
-        print transition_state(empty_room['id'], x['id'])
+        current = end_point
+
+        while current:
+            for value in parents:
+                if current == value:
+                    x = transition_state(get_state(parents[current])['id'], get_state(current)['id'])['event']['effect']
+                    #print(transition_state(get_state(parents[current])['id'], get_state(current)['id']))['action']
+                    
+                    path.append({"from_node" : parents[current], "to_node" : current, "effect": x})
+                    #print("value ", value)
+                    current = parents[value]
+                 
+    path = path[::-1]
+
+    Total_HP = 100
+    for p in path:
+        print (str(get_state(p['from_node'])['location']['name']) + " " + str(p['from_node']) + " : " +  str(get_state(p['to_node'])['location']['name']) + " " +  str(p['to_node']) + " : " + str(p['effect']))
+        Total_HP = Total_HP + p['effect']
+    print("Total HP: " + str(Total_HP))
+
+
